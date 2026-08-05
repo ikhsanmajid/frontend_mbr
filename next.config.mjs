fbc57cd01c5e29a6d070f33df48a68bf5f8e88cd
ikhsanmajid <ikhsanmajid@gmail.com>
2026-08-06 06:18:27 +0700

    tes3

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: false,
-    basePath: '/',
-    assetPrefix: '/',
-    images: { path: '/_next/image', unoptimized: true },
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/',
-                basePath: false,
-                permanent: false
-            }
-        ]
-    }
+
 };
 
 export default nextConfig;
